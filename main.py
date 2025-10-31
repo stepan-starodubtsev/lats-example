a475fa2549d09c9667740374962afc43124d5cd2
Stepan <stepan.starodubtsev.work@gmail.com>
2025-10-31 11:03:34 +0200

    added langfuse tracing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import time
+
 from dotenv import load_dotenv
 
-from src.config import set_env_if_undefined  # ensure env vars present
+from src.config import set_env_if_undefined, langfuse_handler  # ensure env vars present
 from src.graph_build import build_graph
 
 load_dotenv()
@@ -18,17 +20,24 @@ def main():
         "recorded instance for each of the top 5 most common birds."
     )
 
+    start_time = time.time()
     last_step = None
-    for step in graph.stream({"input": question}):
+    for step in graph.stream({"input": question},
+                             config={"callbacks": [langfuse_handler]}):
         last_step = step
         step_name, step_state = next(iter(step.items()))
         print(step_name)
         print("rolled out: ", step_state["root"].height)
         print("---")
 
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+
     solution_node = last_step["expand"]["root"].get_best_solution()
     best_trajectory = solution_node.get_trajectory(include_reflections=False)
     print(best_trajectory[-1].content)
+
+    print(f"Method finished in: {elapsed_time:.4f} seconds.")
 
 
 if __name__ == "__main__":
--- src/config.py
+++ src/config.py
@@ -1,8 +1,28 @@
 import getpass
 import os
 
+from dotenv import load_dotenv
+from langfuse import Langfuse, get_client
+from langfuse.langchain import CallbackHandler
+
+load_dotenv()
+
 
 def set_env_if_undefined(var: str) -> None:
     if os.environ.get(var):
         return
     os.environ[var] = getpass.getpass(var)
+
+
+# Initialize Langfuse handler and client with error handling
+try:
+    _ = Langfuse(
+        secret_key=os.getenv("LANGFUSE_SK"),
+        public_key=os.getenv("LANGFUSE_PK"),
+        host=f"{os.getenv('LANGFUSE_API_URL')}:{os.getenv('LANGFUSE_API_PORT')}",
+        )
+    langfuse = get_client()
+    langfuse_handler = CallbackHandler()
+except Exception as e:
+    print(f"Failed to initialize Langfuse handler or Langfuse Client: {e}")
+    raise
